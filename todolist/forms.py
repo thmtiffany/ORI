c8fc5bc3d3d23fad36853aed52d9df7385ce0e22
Sharlene Quek <sharlene.qk@gmail.com>
2020-07-05 21:26:30 +0800

    modified todolist

--- todolist/forms.py
+++ todolist/forms.py
@@ -1,9 +1,7 @@
 from django import forms
 
-
 class TodoListForm(forms.Form):
     text = forms.CharField(max_length=100,
     widget = forms.TextInput(
     attrs={'class': 'form-control', 'placeholder': 'Insert new task here', 'aria-label':
-    'Todo', 'aria-describedby': 'add-btn'}
-    ))
+    'Todo', 'aria-describedby': 'add-btn'}))
